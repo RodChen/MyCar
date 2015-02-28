1b4925bb5a2fd1705b0a58e970e8c45abd56cf38
Rod Chen <ozaki90@gmail.com>
2015-02-28 15:51:01 +0800

    A guest can search cars.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_car, only: [:edit, :update, :destroy,
     :edit_location, :edit_pictures, :edit_calendar]
   
